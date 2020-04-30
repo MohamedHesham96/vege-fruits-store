2fda60e6b91e248939a5ca3031fe7e3fa6ad9bed
MohamedHesham96 <mohamed.hesham.fci@gmail/com>
2020-04-30 19:31:29 +0200

    Add Balance entity

---
+++ src/main/java/com/bluesoft/vegefruitsstore/entity/Balance.java
@@ -0,0 +1,125 @@
+package com.bluesoft.vegefruitsstore.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "balance")
+public class Balance {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+
+	@Column(name = "item_name")
+	String itemName;
+
+	@Column(name = "count")
+	int count;
+
+	@Column(name = "weight")
+	float weight;
+
+	@Column(name = "kilo_price")
+	float kiloPrice;
+
+	@Column(name = "cash")
+	float cash;
+
+	@Column(name = "later")
+	float later;
+
+	@Column(name = "total_amount")
+	float totalAmount;
+
+	@Column(name = "seller_name")
+	String sellerName;
+
+	@Column(name = "date")
+	String date;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public float getKiloPrice() {
+		return kiloPrice;
+	}
+
+	public void setKiloPrice(float kiloPrice) {
+		this.kiloPrice = kiloPrice;
+	}
+
+	public float getCash() {
+		return cash;
+	}
+
+	public void setCash(float cash) {
+		this.cash = cash;
+	}
+
+	public float getLater() {
+		return later;
+	}
+
+	public void setLater(float later) {
+		this.later = later;
+	}
+
+	public float getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+}
