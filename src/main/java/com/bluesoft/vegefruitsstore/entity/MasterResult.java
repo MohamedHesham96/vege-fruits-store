6a265e57efe34b56e5e3bd0ce7ca35be86aed2a9
MohamedHesham96 <mohamed.hesham.fci@gmail/com>
2020-05-08 16:12:12 +0200

    Add icons

---
+++ src/main/java/com/bluesoft/vegefruitsstore/entity/MasterResult.java
@@ -0,0 +1,24 @@
+package com.bluesoft.vegefruitsstore.entity;
+
+public class MasterResult {
+
+	Double totalCollect;
+	Double totalRelay;
+
+	public Double getTotalCollect() {
+		return totalCollect;
+	}
+
+	public void setTotalCollect(Double totalCollect) {
+		this.totalCollect = totalCollect;
+	}
+
+	public Double getTotalRelay() {
+		return totalRelay;
+	}
+
+	public void setTotalRelay(Double totalRelay) {
+		this.totalRelay = totalRelay;
+	}
+
+}
